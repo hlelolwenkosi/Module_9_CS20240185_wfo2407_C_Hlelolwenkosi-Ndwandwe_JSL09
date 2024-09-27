ae9b6aa65d4348e09ad3651721510a4ea89bf12d
Hlelolwenkosi Ndwandwe <1071hf@gmail.com>
2024-09-27 12:27:40 +0200

    Displayed time on the page

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+function getCurrentTime() {
+ const date = new Date()
+document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+ setInterval(getCurrentTime, 1000)
